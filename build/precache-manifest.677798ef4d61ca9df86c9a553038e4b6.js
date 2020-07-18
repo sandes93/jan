a0c7b28716897f7334110469b348ca7fc8093483
Sandes <sandeshpathak0112@gmail.com>
2020-07-18 12:31:27 +1000

    update

---
+++ build/precache-manifest.677798ef4d61ca9df86c9a553038e4b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428a16a98fea8011a4ceed587363d40a",
+    "url": "/jan/index.html"
+  },
+  {
+    "revision": "3dac22fd96391a1ca880",
+    "url": "/jan/static/js/2.4352b24e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jan/static/js/2.4352b24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84abfbeb47d79cbb74d",
+    "url": "/jan/static/js/main.2c267b7c.chunk.js"
+  },
+  {
+    "revision": "e3abc92946cd15c08b83",
+    "url": "/jan/static/js/runtime-main.489d1a42.js"
+  }
+]);
