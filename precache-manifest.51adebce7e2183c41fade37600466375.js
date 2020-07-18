afecbc403168f3818569d857b5fcb5cd5b503514
Sandes <sandeshpathak0112@gmail.com>
2020-07-18 10:47:43 +1000

    Updates

---
+++ precache-manifest.51adebce7e2183c41fade37600466375.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8472cb41be195ed772bfd68aa23dad",
+    "url": "/IAMJANICE/index.html"
+  },
+  {
+    "revision": "ca3adcf06e625b7e9398",
+    "url": "/IAMJANICE/static/js/2.ad1839b8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/IAMJANICE/static/js/2.ad1839b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65468803713de0c24752",
+    "url": "/IAMJANICE/static/js/main.8ff87707.chunk.js"
+  },
+  {
+    "revision": "12cc5e17d035a6a4e611",
+    "url": "/IAMJANICE/static/js/runtime-main.6636b42f.js"
+  }
+]);
